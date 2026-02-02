dbcc518abfe168d32b37a95c0804c83cde72c224
Jessica Vien <jessicavien@Jessicas-MacBook-Pro.local>
2026-02-02 15:51:27 -0800

    stuff

---
+++ app/test/page.js
@@ -0,0 +1,9 @@
+import DmBox from "@/components/shared/dm"
+
+export default function Home() {
+    return (
+        <div>
+            <DmBox></DmBox>
+        </div>
+    )
+}
---
+++ components/shared/dm/index.js
@@ -0,0 +1,34 @@
+export default function dmBox(){
+    return (
+
+<div style={{
+    backgroundColor: "grey",
+    display: "flex",
+    height: "300px",
+    width: "200px",
+}}> 
+
+    <div style={{
+        backgroundColor: "blue",
+    
+    }}>
+    </div>
+
+    <div style={{
+        backgrounColor: "green",
+       
+    }}>
+        <p>DM</p>
+
+    </div>
+
+    <div>
+    <p>@username</p>
+    <p>bio................</p>
+
+    </div>
+</div>
+
+    )
+}
+
